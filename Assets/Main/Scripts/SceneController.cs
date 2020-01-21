28b5c6d58fbefcf805ed2a6380e66ec4742040f0
JDBU-Games <47919909+jdbugames@users.noreply.github.com>
2020-01-20 19:56:50 -0500

    Day03

---
+++ Assets/Main/Scripts/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void ChangeScene(string str_SceneName)
+    {
+        SceneManager.LoadScene(str_SceneName);
+    }
+
+    public void QuitAPP()
+    {
+        Application.Quit();
+    }
+}
